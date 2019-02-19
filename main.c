c33c302e24732654aed00a09d99be618596ed314
JakandDaxter <aliana124@gmail.com>
2019-02-19 00:39:23 -0500

    Ch 1 & 2
    Enabled output for channel one and two. Will connect to LED's to see the output later.

--- main.c
+++ main.c
@@ -5,11 +5,8 @@
 #include "SysClock.h"
 #include "UART.h"
 #include "LED.h"
-#include "States.h"
-#include "Timer_3_PIT.h"
-#include "Stack.h"
-#include "Servo_CL_Compiler.h"
-#include "Timer_5_PIT.h"
+#include "Timer_2_PWM.h"
+
 
 uint8_t buffer[BufferSize];
 
@@ -20,55 +17,9 @@ int main(void)
 	System_Clock_Init(); 
 	LED_Init();
 	UART2_Init();
-	Timer3_Init();
+	Init_Timer2();
 	__enable_irq();
-	start_timer5();
-	unsigned char UserInput[10];
-	Print("Welcome to the Servo Control system\r\n");
-	Print("The system simultaneously controls two servos.\n\rEach servo has a dedicated command script that has been preloaded.\n\r");
-	Print("You are able to START, PAUSE, and CONTINUE the execution.\n\rOr move either servo LEFT or RIGHT 1 position.\n\r");
-	while(1)
-	{
-		int j = 0;
-		Red_LED_Off();
-		Green_LED_Off();
-		Print(">");
-		unsigned char USART_char = '0';
-		while(USART_char != '\r')
-		{
-			USART_char = USART_Read(USART2);
-			USART_Write(USART2,&USART_char,1);
-			UserInput[j] = USART_char;
-			j++;
-		}
-		Print("\n\r");
-		if((UserInput[0] == 'B') || (UserInput[0] == 'b'))
-		{
-			start_timer3();	
-		}
-		else if((UserInput[0] == 'P') || (UserInput[0] == 'p'))
-		{
-			stop_timer3();
-		}
-		else if((UserInput[0] == 'C') || (UserInput[0] == 'c'))
-		{
-			restart_timer3();
-		} 
-		else{Print("Override Not Available\n\r");}
-		/*Servo 2 control*/
-		
-		if((UserInput[1] == 'B') || (UserInput[1] == 'b'))
-		{
-			start_timer5();	
-		}
-		else if((UserInput[1] == 'P') || (UserInput[1] == 'p'))
-		{
-			stop_timer5();
-		}
-		else if((UserInput[1] == 'C') || (UserInput[1] == 'c'))
-		{
-			restart_timer5();
-		} 
-		else{Print("Override Not Available\n\r");}
-	}
+	
+	USART_Write(USART2, buffer, );
+	
 }
