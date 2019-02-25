17744ad6357a2e7875c42edf2c58fa91baa7079a
Aliana Tejeda <aliana124@gmail.com>
2019-02-25 17:54:16 -0500

    Functions to change servo

---
+++ Servo_Communication.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+
+#include "stm32l476xx.h"
+#include "SysClock.h"
+#include "LED.h"
+#include "Servo_Communication.h"
+
+
+//function to clear and reprogram the servo with entered angle
+
+void AngleThrow ()
+{
+	
+}
---
+++ Servo_Communication.h
@@ -0,0 +1,12 @@
+#ifndef __TIMER_2_INPUT_CAPTURE_H
+#define __TIMER_2_INPUT_CAPTURE_H
+
+#include "stm32l476xx.h"
+
+void Init_Timer2(void);
+void Start_Timer2(void);
+void Stop_Timer2(void);
+void Init_GPIO(void);
+
+
+#endif /*__TIMER_2_INPUT_CAPTURE_H*/ 
